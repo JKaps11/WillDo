f228d729358fd9b095fe6b8a52f209eb90008763
Joshua Kaplan <joshua.kaplan@syn-data.com>
2025-12-15 04:16:44 -0500

    project initialization

---
+++ src/types/error.ts
@@ -0,0 +1,5 @@
+export interface AppError {
+    message: string;
+    data: object;
+    code?: number;
+}
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+import neon from './neon-vite-plugin.ts'
+
+const config = defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    devtools(),
+    netlify(),
+    neon,
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: 'src',
+    }),
+    react(),
+  ],
+})
+
+export default config
